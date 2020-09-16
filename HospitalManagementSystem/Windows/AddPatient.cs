32535b0f7ce00dc858f487cde4a8b0a1f70689e4
snigdho611 <snigdho611@users.noreply.github.com>
2020-09-16 11:04:17 +0600

    Debugging AddPatient form

--- HospitalManagementSystem/Windows/AddPatient.cs
+++ HospitalManagementSystem/Windows/AddPatient.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Oracle.ManagedDataAccess.Client;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,6 +20,36 @@ namespace HospitalManagementSystem.Windows
 
         private void btnAddPatient_Click(object sender, EventArgs e)
         {
+            try
+            {
+                string connectionString = "Data Source=localhost;User ID=SNIGDHO;Password=student;";
+                OracleConnection connection = new OracleConnection(connectionString);
+
+
+
+                //OracleCommand command = new OracleCommand("INSERT INTO PATIENTS(pat_name, gender, age, diagnosis, doc_id, room_no) values(:pName, :pGender, :pAge, :pDiagnosis, :pDocId, :pRoomNo)", connection);
+                OracleCommand command = new OracleCommand("INSERT INTO PATIENTS(pat_name) values(:pName)", connection);
+                connection.Open();
+
+                command.Parameters.Add("pName", OracleDbType.Varchar2).Value = "ABCD";
+                    //command.Parameters.Add("pGender", OracleDbType.Varchar2).Value = "ABCD";
+                    //cmd.Parameters.Add("p3", OracleDbType.Varchar2).Value = txtpass.Text;
+                    int rowsUpdated = command.ExecuteNonQuery();
+                    if (rowsUpdated == 0)
+                    {
+                        MessageBox.Show("Record not inserted");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Success!");
+                        MessageBox.Show("User has been created");
+                    }
+                
+            }
+            catch (Exception E)
+            {
+
+            }
 
         }
     }
