ae7704ea6ee347e35f437b77a705a66d472706c2
snigdho611 <snigdho611@users.noreply.github.com>
2020-09-18 12:57:45 +0600

    sql update

--- HospitalManagementSystem/Windows/AddPatient.cs
+++ HospitalManagementSystem/Windows/AddPatient.cs
@@ -25,31 +25,49 @@ namespace HospitalManagementSystem.Windows
                 string connectionString = "Data Source=localhost;User ID=SNIGDHO;Password=student;";
                 OracleConnection connection = new OracleConnection(connectionString);
 
-                string insertPatientQuery = "begin insertPatient('"
-                    + txtName.Text + "', '"
-                    + cmbGender.Text.ToString() + "', "
-                    + txtAge.Text + ", '"
-                    + txtDgn.Text + "', "
-                    + txtDocId.Text + ", "
-                    + txtRoom.Text + ", "
-                    + txtBill.Text + "); end;";
+                /*
+                string insertPatientQuery;
 
-               // string insertPatientQuery = "begin insertPatient(:p1, :p2, :p3, :p4, :p5, :p6, :p7); end;";
+                if (String.IsNullOrWhiteSpace(txtDgn.Text) == true)
+                {
+                    insertPatientQuery = "begin insertPatient('"
+                      + txtName.Text + "', '"
+                      + cmbGender.Text.ToString() + "', "
+                      + txtAge.Text + ", 'N/A', "
+                      + txtDocId.Text + ", "
+                      + txtRoom.Text + ", "
+                      + txtBill.Text + "); end;";
+                }
+                else
+                {
+                    insertPatientQuery = "begin insertPatient('"
+                      + txtName.Text + "', '"
+                      + cmbGender.Text.ToString() + "', "
+                      + txtAge.Text + ", '"
+                      + txtDgn.Text + "', "
+                      + txtDocId.Text + ", "
+                      + txtRoom.Text + ", "
+                      + txtBill.Text + "); end;";
+                }*/
+
+                
+
+                string insertPatientQuery = "begin insertPatient(\n:p1, \n:p2, \n:p3, :p4, :p5, :p6, :p7); end;";
 
                 OracleCommand command = new OracleCommand(
                    insertPatientQuery, connection);
                 connection.Open();
 
-                txtBill.Text = insertPatientQuery;
-                /*
-                command.Parameters.Add("p1", OracleDbType.NVarchar2).Value = txtName.Text;
-                command.Parameters.Add("p2", OracleDbType.NVarchar2).Value = cmbGender.Text.ToString();
-                command.Parameters.Add("p3", OracleDbType.NVarchar2).Value = Convert.ToInt32(txtAge.Text);
-                command.Parameters.Add("p4", OracleDbType.NVarchar2).Value = txtDgn.Text;
-                command.Parameters.Add("p5", OracleDbType.NVarchar2).Value = Convert.ToInt32(txtDocId.Text);
-                command.Parameters.Add("p6", OracleDbType.NVarchar2).Value = Convert.ToInt32(txtRoom.Text);
-                command.Parameters.Add("p7", OracleDbType.NVarchar2).Value = Convert.ToInt32(txtBill.Text);
-                */
+                //txtBill.Text = insertPatientQuery;
+                
+                command.Parameters.Add("p1", OracleDbType.Varchar2).Value = txtName.Text;
+                command.Parameters.Add("p2", OracleDbType.Varchar2).Value = cmbGender.Text.ToString();
+                command.Parameters.Add("p3", OracleDbType.Varchar2).Value = txtAge.Text;
+                command.Parameters.Add("p4", OracleDbType.Varchar2).Value = txtDgn.Text;
+                command.Parameters.Add("p5", OracleDbType.Varchar2).Value = txtDocId.Text;
+                command.Parameters.Add("p6", OracleDbType.Varchar2).Value = txtRoom.Text;
+                command.Parameters.Add("p7", OracleDbType.Varchar2).Value = txtBill.Text;
+                
                 txtBill.Text = insertPatientQuery;
 
                 int rowsUpdated = command.ExecuteNonQuery();
