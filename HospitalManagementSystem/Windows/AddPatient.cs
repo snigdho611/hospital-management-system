b5e2a15bad1e870e131c615945c8c84fc814a723
snigdho611 <snigdho611@users.noreply.github.com>
2020-09-24 11:40:35 +0600

    small updates to database

--- HospitalManagementSystem/Windows/AddPatient.cs
+++ HospitalManagementSystem/Windows/AddPatient.cs
@@ -1,12 +1,5 @@
 ﻿using System;
 using Oracle.ManagedDataAccess.Client;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace HospitalManagementSystem.Windows
